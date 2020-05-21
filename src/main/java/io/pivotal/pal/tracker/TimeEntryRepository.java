fbb920835d46860983caa87dcfac66b4a9c5b5d9
Imad Quadri <squadri@gmail.com>
2020-05-21 18:16:35 +0000

    Spring JDBC Template

--- src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
@@ -3,13 +3,13 @@ package io.pivotal.pal.tracker;
 import java.util.List;
 
 public interface TimeEntryRepository {
-    public TimeEntry create(TimeEntry any);
+    TimeEntry create(TimeEntry any);
 
-    public TimeEntry find(long timeEntryId);
+    TimeEntry find(long timeEntryId);
 
-    public List<TimeEntry> list();
+    List<TimeEntry> list();
 
-    public TimeEntry update(long eq, TimeEntry any);
+    TimeEntry update(long eq, TimeEntry any);
 
-    public void delete(long timeEntryId);
+    void delete(long timeEntryId);
 }
